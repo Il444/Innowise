b9198ecac91446dcaefb8b8fbe835904eb055c21
Il444 <ifedorenko08@icloud.com>
2025-11-12 20:50:18 +0300

    Add main project logic with colorama

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("dsfds")
+from colorama import Fore, Back, Style, init
+
+init(autoreset=True)
+
+
+def main():
+    print(Fore.GREEN + "=== Добро пожаловать в мой проект! ===")
+    print(Fore.BLUE + "Этот текст синего цвета")
+    print(Fore.RED + "А этот - красного")
+    print(Fore.YELLOW + Back.BLACK + "Желтый текст на черном фоне")
+
+    name = input(Fore.CYAN + "Как вас зовут? ")
+    print(Fore.MAGENTA + f"Привет, {name}!")
+
+    print(Style.BRIGHT + Fore.WHITE + "Спасибо за использование программы!")
+
+
+if __name__ == "__main__":
+    main()
