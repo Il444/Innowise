8e8dd184f2131e320e225089bb2c459479b5f901
Il444 <ifedorenko08@icloud.com>
2026-08-08 19:52:06 +0300

    added functions to improve structure, improved user's feedback

---
+++ lecture_2/mini-profile generator.py
@@ -0,0 +1,49 @@
+def generate_profile(age):
+    if 0 <= age <= 12:
+        return "Child"
+    elif 13 <= age <= 19:
+        return "Teenager"
+    elif age >= 20:
+        return "Adult"
+
+def get_birth_year():
+    while True:
+        try:
+            birth_year_str = input("Enter your birth year: ").strip()
+            birth_year = int(birth_year_str)
+            if 1900 <= birth_year <= 2026:
+                return birth_year
+            print("Please enter a realistic year (e.g. 1995).")
+        except ValueError:
+            print("Please enter a valid number.")
+
+def main():
+    user_name = input("Enter your full name: ")
+    current_age = 2026 - birth_year
+    hobbies = []
+    while True:
+        hobby = input("Enter a favorite hobby or type 'stop' to finish: ")
+        if hobby.lower() == "stop":
+            break
+        hobbies.append(hobby)
+
+    life_stage = generate_profile(current_age)
+    dict = {"Name" : user_name, "Age" : current_age, "Life Stage" : life_stage, "Favorite Hobby" : hobbies}
+
+    print("---")
+    print("Profile Summary:")
+    for key, value in dict.items():
+        if key == "Favorite Hobby":
+            if not value:
+                print("You didn't mention any hobbies.")
+            else:
+                print(f"Favorite hobbies ({len(value)}) :")
+                for hobby in value:
+                    print(f"- {hobby}")
+        else:
+            print(f"{key}: {value}")
+
+    print("---")
+
+if __name__ == "__main__":
+    main()
