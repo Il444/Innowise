685db283ebc4eb0a7dae867a4417646a7519bad3
Il444 <ifedorenko08@icloud.com>
2026-08-09 19:32:56 +0300

    Working prototype

--- lecture_3/the student grade analyzer.py
+++ lecture_3/the student grade analyzer.py
@@ -2,48 +2,53 @@ def add_grades(students_base):
     student_name = input("Enter student name: ")
     for student in students_base:
         if student["name"] == student_name:
-            grade = int(input("Enter a grade (or 'done' to finish):"))
-            if 0 <= grade <= 100:
-                student["grades"].append(grade)
-            else:
-                print("Invalid grade. Try adain!")
-        else:
-            print("Student you are looking for does not exist")
+            while True:
+                feedback = input("Enter a grade (or 'done' to finish):")
+                if feedback == "done":
+                    break
+                try:
+                    grade = float(feedback)
+                    if 0 <= grade <= 100:
+                        student["grades"].append(grade)
+                    else:
+                        print("Invalid grade. Try adain!")
+                except ValueError:
+                    print("Invalid input. Please enter a number.")
+            return
+    print("Student you are looking for does not exist")
+
 
 def add_student(students_base):
     student_name = input("Enter student name: ")
-    if student_name in students_base:
-        print("Student already exists")
-    else:
-        student_profile = {"name": student_name, "grades": []}
-        students_base.append(student_profile)
+    for student in students_base:
+        if student_name.lower() == student["name"].lower():
+            print("Student already exists")
+            return
+    student_profile = {"name": student_name, "grades": []}
+    students_base.append(student_profile)
 
 def get_student_average(student):
-    if student["grades"] != []:
-        student_average = 0
-        for grade in student["grades"]:
-            student_average += grade
-        student_average /= len(student["grades"])
-    else:
-        student_average = "N/A"
-    return student_average
+    if student["grades"]:
+        return sum(student["grades"])/len(student["grades"])
+    return "N/A"
 
 def get_info(students_base, parameter):
     max_average = 0
     min_average = 100
     overall_average = 0
+    count = 0
     for student in students_base:
-        if student["grades"] != []:
-            for student in students_base:
-                student_average = get_student_average(student)
+        if student["grades"]:
+            count += 1
+            student_average = get_student_average(student)
+            if student_average != "N/A":
                 overall_average += student_average
                 if max_average < student_average:
                     max_average = student_average
                 if min_average > student_average:
-                    max_average = student_average
-        else:
-            student_average = "N/A"
-    overall_average /= len(students_base["name"])
+                    min_average = student_average
+
+    overall_average /= count
     if parameter == "overall_average":
         return overall_average
     elif parameter == "max_average":
@@ -58,9 +63,8 @@ def create_and_print_profile(students_base):
         min_average = get_info(students_base, "min_average")
         overall_average = get_info(students_base, "overall_average")
         for student in students_base:
-            for student in students_base:
-                student_average = get_student_average(student)
-                print(f"{student["name"]}'s average grade is {student_average}")
+            student_average = get_student_average(student)
+            print(f"{student["name"]}'s average grade is {student_average}")
         print("---------------------")
         print(f"Max Average: {max_average}")
         print(f"Min Average: {min_average}")
@@ -77,13 +81,13 @@ def find_and_print_top_student(students_base):
 
 def main():
     students_base = []
-    print("--- Student Grade Analyzer ---")
     while True:
+        print("--- Student Grade Analyzer ---")
         print("1. Add a new student"
-              "2. Add grades for a student"
-              "3. Generate a full report"
-              "4. Find the top student"
-              "5. Exit program")
+              "\n2. Add grades for a student"
+              "\n3. Generate a full report"
+              "\n4. Find the top student"
+              "\n5. Exit program")
         choice = input("Enter your choice: ")
         try:
             choice = int(choice)
@@ -95,6 +99,11 @@ def main():
                 create_and_print_profile(students_base)
             elif choice == 4:
                 find_and_print_top_student(students_base)
+            elif choice == 5:
+                print("Exiting program.")
+                break
+            else:
+                print("Incorrect choice. Please enter 1-5.")
         except ValueError:
             print("Invalid input. Please try a number.")
         except Exception as e:
